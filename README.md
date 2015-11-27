ec3513bf3c5ec63f169936e6c120bcbd259aa843
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2015-11-28 00:18:24 +0100

    Part 1

---
+++ README.md
@@ -0,0 +1,5 @@
+Prerequisites
+-------------
+
+- [Visual Studio 2015](https://www.visualstudio.com/en-us/products/visual-studio-community-vs.aspx)
+- [ASP.NET 5](https://docs.asp.net/en/latest/getting-started/installing-on-windows.html)
---
+++ Sample.Front/wwwroot/app/app.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', [
+        // Angular modules 
+
+        // Custom modules 
+
+        // 3rd Party Modules
+        'ui.router'
+    ])
+    .config(function ($urlRouterProvider, $stateProvider) {
+        // For any unmatched url, redirect to /home
+        $urlRouterProvider.otherwise("/home");
+        // Now set up the states
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "app/app.template.html",
+                controller: "HomeController",
+                controllerAs: "home"
+            });
+    })
+    .controller('HomeController', HomeController);
+
+    function HomeController() {
+        var vm = this;
+        vm.title = 'Hello world';
+    };
+})();
